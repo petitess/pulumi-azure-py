a679e8f7f072fefd17cc2ce6b071e3a9ee051105
Karol <93782725+petitess@users.noreply.github.com>
2025-12-29 14:51:56 +0100

    condition

--- xxx01/condition.py
+++ xxx01/condition.py
@@ -1,1 +1,14 @@
 name="Standard" if kv['sku'].lower() == "standard" else "Premium",
+
+ip_configurations=(
+    [
+        network.PrivateEndpointIPConfigurationArgs(
+            name="config",
+            group_id="vault",
+            member_name="default",
+            private_ip_address=kv.get("private_ip"),
+        )
+    ]
+    if kv.get("private_ip")
+    else None
+),
